c86a8b4ae6caa5b656ce6a0b635b2930eb4d1910
Max Lang <maxwgl@gmail.com>
2012-10-03 18:23:25 -0700

    setup stuff

--- environment-setup.ps1
+++ environment-setup.ps1
@@ -9,9 +9,6 @@ source activate
 ./activate
 popd
 
-$env:VS90COMNTOOLS=$env:VS100COMNTOOLS
-VS90COMNTOOLS=$VS100COMNTOOLS
-
 pip install hg+https://bitbucket.org/wkornewald/django-nonrel
 pip install hg+https://bitbucket.org/wkornewald/djangotoolbox
 pip install git+https://github.com/django-nonrel/mongodb-engine
