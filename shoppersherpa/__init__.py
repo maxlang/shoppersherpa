7f3e6163bf6424310976709bcde882ab1b69ca0f
Max Lang <maxwgl@gmail.com>
2012-11-01 22:36:11 -0700

    moved models to models directory

---
+++ shoppersherpa/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 01 15:28:58 2012
+
+@author: Max Lang
+"""
+
---
+++ shoppersherpa/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 01 15:27:17 2012
+
+@author: Max Lang
+"""
