b5b4d1fea05eb1d8c21c32d504f13b0b288be5a8
Max Lang <maxwgl@gmail.com>
2012-11-02 00:09:53 -0700

    rearranged a bit, added view, added run.py

---
+++ shoppersherpa/frontend/__init__.py
@@ -0,0 +1,8 @@
+'''
+Created on Nov 1, 2012
+
+@author: Max Lang
+'''
+from bottle import BaseTemplate
+
+BaseTemplate.extensions.append("jinja")
---
+++ shoppersherpa/run.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from frontend import controllers
+
+if __name__ == "__main__":
+    controllers.start()
