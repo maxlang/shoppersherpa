4d42c6675de85109275266edf4821a6626218c76
Spencer Rarrick <rarrick@hotmail.com>
2012-11-13 12:41:59 -0800

    attribute stuff

--- shoppersherpa/analysis/attributes/attributeinfo.py
+++ shoppersherpa/analysis/attributes/attributeinfo.py
@@ -1,9 +1,11 @@
 from mongoengine import (
      DictField, ListField, StringField, DecimalField, IntField, MapField,
-     DateTimeField, URLField, EmbeddedDocument, EmbeddedDocumentField, 
-     DynamicDocument)
+     BooleanField, DateTimeField, URLField, EmbeddedDocument,
+     EmbeddedDocumentField, DynamicDocument)
 
-from parsing import ParsedProduct
+from models.models import (ParsedProduct, AttrInfo)
+import numpy
+#from shoppersherpa.models import ParsedProduct
 
 
 class AttrValueStats(EmbeddedDocument):
@@ -12,22 +14,14 @@ class AttrValueStats(EmbeddedDocument):
     median_price = DecimalField()
     mean_rating = DecimalField()
     median_rating = DecimalField()
+    price_std_dev = DecimalField()
+    rating_std_dev = DecimalField()
 
 
 class AttrStats(EmbeddedDocument):
     count = IntField()
     values = DictField()
-
-
-class AttrInfo(DynamicDocument):
-    name = StringField()
-    rank = IntField()
-    stats = AttrStats()
-    meta = {'allow_inheritance': True}
-
-
-#def sum_func(obj, prev):
-#    
+    
 
 def sum_field(doc_set, field):
     total = 0
@@ -35,15 +29,32 @@ def sum_field(doc_set, field):
         attr = d['attr']
         if field in attr:
             total += attr[field]
-            
+    return total
+
+
+def convert_to_vector(doc_set, field):
+    ls = []
+    for prod in doc_set:
+        attr = prod['attr']
+        if field in attr and attr[field] is not None:
+            ls.append(attr[field])
+    return numpy.array([ls])
+
+
 def find_median(doc_set, field):
     vals = [x['attr'][field] for x in doc_set]
     vals.sort()
+    if len(vals) == 0:
+        return None
+    elif len(vals) % 1 == 1:
+        return vals[len(vals) / 2]
+    else:
+        return (vals[(len(vals) / 2) - 1] + vals[(len(vals) / 2)]) / 2
 
 
 if __name__ == "__main__":
-    #prods = ParsedProduct.objects.select_related()
-    #prods = ParsedProduct.objects
+    print 'deleting'
+    AttrInfo.objects.delete()
 
     use_attrs = ['tvType', 'screenSizeIn']
 
@@ -67,19 +78,32 @@ if __name__ == "__main__":
         for val in values:
             avs = AttrValueStats()
             val_set = ParsedProduct.objects(**{filter_attr_str: val})
-            avs.count = val_set.count()
-            #sum_price = ParsedProduct.objects.group({key: {"regularPrice": True},
-            #                         cond: {full_attr_str: val},
-            #                         reduce: function()
-            #                          )
-            avs.mean_price = sum_field(val_set, 'regularPrice') / avs.count
-            
+            price_vec = convert_to_vector(ParsedProduct.objects(**{filter_attr_str: val}), 'regularPrice')
+            rating_vec = convert_to_vector(ParsedProduct.objects(**{filter_attr_str: val}), 'customerReviewAverage')
+            cutoff_rating_vec = convert_to_vector(ParsedProduct.objects(
+                                                                        **{filter_attr_str: val, 
+                                                                           'customerReviewCount': {"$gte": 10}}), 
+                                                  'customerReviewAverage')
+            #rating_vec = price_vec
+            avs.count = len(price_vec)
+
+            avs.mean_price = numpy.mean(price_vec)
+            avs.median_price = numpy.median(price_vec)
+            avs.mean_rating = numpy.mean(rating_vec)
+            avs.median_rating = numpy.median(rating_vec)
+            avs.price_std_dev = numpy.std(price_vec)
+            avs.rating_std_dev = numpy.std(rating_vec)
+
+            print "{6}: {0} {1} {2} {3} {4} {5}".format(avs.mean_price,
+                                                        avs.median_price,
+                                                        avs.mean_rating,
+                                                        avs.median_rating,
+                                                        avs.price_std_dev,
+                                                        avs.rating_std_dev,
+                                                        val)
+
             new_ai.stats.values[val] = avs
-
-            #map_func = "function() {emit('xx', this.regularPrice)}"
-            #reduce_func = "function(key, values) {var sum=0; for (var i=0; i<values. length; i++) {sum += values[i];} return sum;}"
-            #ParsedProduct.objects.map_reduce(map_func, reduce_func, output='mapreduceout')
-
+            new_ai.save()
 
             print "{0} {1}".format(val, new_ai.stats.values[val].count)
 
---
+++ shoppersherpa/analysis/attributes/populate_attrinfo.py
@@ -0,0 +1,128 @@
+from models.models import (AttrInfo, Product, ParsedProduct)
+import numpy
+#from shoppersherpa.models import ParsedProduct
+
+
+def fallbackNormalize(prod, attrlist):
+    for x in attrlist:
+        if x in prod.attr:
+            return prod.attr[x]
+
+    return None
+
+
+def normalize3d(prod):
+    name = prod.attr['name'].lower()
+    return "3d" in name
+
+if __name__ == "__main__":
+    size_ai = AttrInfo()
+    size_ai.name = "size"
+    size_ai.display_name = "Screen Size"
+    size_ai.is_discrete = False
+    size_ai.is_independant = True
+    size_ai.rank = 1
+    size_ai.units = "inches"
+
+    brand_ai = AttrInfo()
+    brand_ai.name = "brand"
+    brand_ai.display_name = "Brand"
+    brand_ai.is_discrete = True
+    brand_ai.is_independant = True
+    brand_ai.rank = 2
+    brand_ai.units = None
+
+    tv_type_ai = AttrInfo()
+    tv_type_ai.name = "tv_type"
+    tv_type_ai.display_name = "Television Type"
+    tv_type_ai.is_discrete = True
+    tv_type_ai.is_independant = True
+    tv_type_ai.rank = 3
+    tv_type_ai.units = None
+
+    res_ai = AttrInfo()
+    res_ai.name = "resolution"
+    res_ai.display_name = "Resolution"
+    res_ai.is_discrete = True
+    res_ai.is_independant = True
+    res_ai.rank = 4
+    res_ai.units = None
+
+    refresh_ai = AttrInfo()
+    refresh_ai.name = "refresh"
+    refresh_ai.display_name = "Refresh"
+    refresh_ai.is_discrete = True
+    refresh_ai.is_independant = True
+    refresh_ai.rank = 5
+    refresh_ai.units = "Hertz"
+
+    is_3d_ai = AttrInfo()
+    is_3d_ai.name = "is_3d"
+    is_3d_ai.display_name = "Supports 3D"
+    is_3d_ai.is_discrete = True
+    is_3d_ai.is_independant = True
+    is_3d_ai.rank = 6
+
+    color_ai = AttrInfo()
+    color_ai.name = "color"
+    color_ai.display_name = "Color"
+    color_ai.is_discrete = True
+    color_ai.is_independant = True
+    color_ai.rank = 7
+    color_ai.save()
+
+    num_rating_ai = AttrInfo()
+    num_rating_ai.name = "ratings_num"
+    num_rating_ai.name = "Number of Ratings"
+    num_rating_ai.is_discrete = False
+    num_rating_ai.is_independant = False
+    num_rating_ai.save()
+
+    avg_rating_ai = AttrInfo()
+    avg_rating_ai.name = "ratings_avg"
+    avg_rating_ai.name = "Average Ratings"
+    avg_rating_ai.is_discrete = False
+    avg_rating_ai.is_independant = False
+    avg_rating_ai.save()
+
+    price_ai = AttrInfo()
+    price_ai.name = "price"
+    price_ai.name = "Price"
+    price_ai.is_discrete = False
+    price_ai.is_independant = False
+    price_ai.save()
+
+    ai_elements = [(size_ai, lambda x: fallbackNormalize(x, ['screenSizeIn', 'screenSizeClassIn'])),
+                   (brand_ai, lambda x: fallbackNormalize(x, ['manufacturer'])),
+                   (tv_type_ai, lambda x: fallbackNormalize(x, ['tvType'])),
+                   (res_ai, lambda x: fallbackNormalize(x, ['verticalResolution'])),
+                   (refresh_ai, lambda x: fallbackNormalize(x, ['screenRefreshRateHz'])),
+                   (is_3d_ai, normalize3d),
+                   (color_ai, lambda x: fallbackNormalize(x, ['color'])),
+                   (num_rating_ai, lambda x: fallbackNormalize(x, ['customerReviewCount'])),
+                   (avg_rating_ai, lambda x: fallbackNormalize(x, ['customerReviewAverage'])),
+                   (price_ai, lambda x: fallbackNormalize(x, ['regularPrice']))]
+
+    AttrInfo.objects.delete()
+    print ("cleared AttrInfo table")
+    #Product.objects.selectRelated()
+
+    for prod in Product.objects:
+
+        for ai_tup in ai_elements:
+            ai = ai_tup[0]
+            norm_func = ai_tup[1]
+
+            prod.normalized[ai.name] = norm_func(prod)
+        prod.save()
+
+    print("normalized products")
+
+    for ai_tup in ai_elements:
+        ai = ai_tup[0]
+        vals = Product.objects.distinct(ai.name)
+        #vals.sort()
+        ai.values = vals
+        ai.save()
+
+    print ("saved attribute meta data")
